e6bb9ed019678c9135b65efd1dd10921b1b15449
Narasimha Teja Reddy <reddy.nar@northeastern.edu>
2024-03-01 01:55:51 -0700

    loading spinner

---
+++ frontend/src/Spinner.jsx
@@ -0,0 +1,12 @@
+import './Spinner.css'
+
+const Spinner = () => {
+    return (
+      <div className="spinner">
+        <div className="spinner__text">Loading</div>
+      </div>
+    );
+  };
+  
+export default Spinner;
+  
